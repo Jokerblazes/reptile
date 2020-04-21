11d599b477b7d6c8d1d7c2fc075694da2a81da06
joker <cdzheng@thoughtworks.com>
2020-04-21 17:49:15 +0800

    add getter

--- adapter/api/SeleniumPokemonGetter.go
+++ adapter/api/SeleniumPokemonGetter.go
@@ -5,7 +5,6 @@ import (
 	"github.com/tebeka/selenium"
 	"github.com/tebeka/selenium/chrome"
 	"reptile/domain/model"
-	"time"
 )
 
 type SeleniumPokemonGetter struct {
@@ -20,15 +19,85 @@ func (getter SeleniumPokemonGetter) Pokemons() []model.Pokemon {
 	return pokemons
 }
 
-func (getter SeleniumPokemonGetter) pokemonDetailHtml(number int) interface{} {
-	return nil
+func (getter SeleniumPokemonGetter) pokemonDetailHtml(number int) map[string]interface{} {
+	const (
+		seleniumPath = `./chromedriver`
+		port         = 9515
+	)
+
+	opts := []selenium.ServiceOption{}
+
+	service, err := selenium.NewChromeDriverService(seleniumPath, port, opts...)
+	if nil != err {
+		fmt.Println("start a chromedriver service falid", err.Error())
+	}
+	defer service.Stop()
+
+	caps := selenium.Capabilities{
+		"browserName": "chrome",
+	}
+
+	imagCaps := map[string]interface{}{
+		"profile.managed_default_content_settings.images": 2,
+	}
+	chromeCaps := chrome.Capabilities{
+		Prefs: imagCaps,
+		Path:  "",
+		Args: []string{
+			//"--headless",
+			//"--no-sandbox",
+			"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36", // 模擬user-agent，防反爬
+		},
+	}
+	//以上是設置瀏覽器參數
+	caps.AddChrome(chromeCaps)
+
+	// 重新調起chrome瀏覽器
+	webView, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	if err != nil {
+		fmt.Println("connect to the webDriver faild", err.Error())
+	}
+	defer webView.Close()
+	//打開一個網頁
+	err = webView.Get(fmt.Sprintf("%s%d", "https://pokedex.org/#/pokemon/", number))
+	if err != nil {
+		fmt.Println("get page faild", err.Error())
+	}
+	minutiaMap := make(map[string]string)
+	statsMap := make(map[string]string)
+
+	headerElement, _ := webView.FindElement(selenium.ByClassName, "detail-panel-header")
+	statsElements, _ := webView.FindElements(selenium.ByClassName, "detail-stats-row")
+	minutiaElements, _ := webView.FindElements(selenium.ByClassName, "monster-minutia")
+	name, _ := headerElement.Text()
+	for _, stats := range statsElements {
+		keyValueElement, _ := stats.FindElements(selenium.ByTagName, "span")
+		key, _ := keyValueElement[0].Text()
+		value, _ := keyValueElement[1].Text()
+		statsMap[key] = value
+	}
+
+	for _, minutia := range minutiaElements {
+		keys, _ := minutia.FindElements(selenium.ByTagName, "strong")
+		values, _ := minutia.FindElements(selenium.ByTagName, "span")
+		for i := 0; i < len(keys); i++ {
+			key, _ := keys[i].Text()
+			value, _ := values[i].Text()
+			minutiaMap[key] = value
+		}
+	}
+	return map[string]interface{}{
+		"header":  name,
+		"status":  statsMap,
+		"minutia": minutiaMap,
+	}
 }
 
-func (getter SeleniumPokemonGetter) generatorHtmlToPokemon(selectionMap interface{}, number int) model.Pokemon {
+func (getter SeleniumPokemonGetter) generatorHtmlToPokemon(pokemonMap map[string]interface{}, number int) model.Pokemon {
 	return model.Pokemon{}
 }
 
-func pokemonDetailHtml() {
+func PokemonDetailHtml() {
 	const (
 		seleniumPath = `./chromedriver`
 		port         = 9515
@@ -62,42 +131,41 @@ func pokemonDetailHtml() {
 	//以上是設置瀏覽器參數
 	caps.AddChrome(chromeCaps)
 
-	// 調起chrome瀏覽器
-	w_b1, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	// 重新調起chrome瀏覽器
+	webView, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
 	if err != nil {
 		fmt.Println("connect to the webDriver faild", err.Error())
 		return
 	}
-	//關閉一個webDriver會對應關閉一個chrome窗口
-	//但是不會導致seleniumServer關閉
-	defer w_b1.Quit()
-	err = w_b1.Get("https://pokedex.org/#/pokemon/1")
+	defer webView.Close()
+	//打開一個網頁
+	err = webView.Get("https://pokedex.org/#/pokemon/1")
 	if err != nil {
 		fmt.Println("get page faild", err.Error())
 		return
+	}
+	minutiaMap := make(map[string]string)
+	statsMap := make(map[string]string)
+
+	headerElement, _ := webView.FindElement(selenium.ByClassName, "detail-panel-header")
+	statsElements, _ := webView.FindElements(selenium.ByClassName, "detail-stats-row")
+	minutiaElements, _ := webView.FindElements(selenium.ByClassName, "monster-minutia")
+	name, _ := headerElement.Text()
+	for _, stats := range statsElements {
+		keyValueElement, _ := stats.FindElements(selenium.ByTagName, "span")
+		key, _ := keyValueElement[0].Text()
+		value, _ := keyValueElement[1].Text()
+		statsMap[key] = value
 	}
 
-	// 重新調起chrome瀏覽器
-	w_b2, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-	if err != nil {
-		fmt.Println("connect to the webDriver faild", err.Error())
-		return
+	for _, minutia := range minutiaElements {
+		keys, _ := minutia.FindElements(selenium.ByTagName, "strong")
+		values, _ := minutia.FindElements(selenium.ByTagName, "span")
+		for i := 0; i < len(keys); i++ {
+			key, _ := keys[i].Text()
+			value, _ := values[i].Text()
+			minutiaMap[key] = value
+		}
 	}
-	defer w_b2.Close()
-	//打開一個網頁
-	err = w_b2.Get("https://www.toutiao.com/")
-	if err != nil {
-		fmt.Println("get page faild", err.Error())
-		return
-	}
-	//打開一個網頁
-	err = w_b2.Get("https://www.baidu.com/")
-	if err != nil {
-		fmt.Println("get page faild", err.Error())
-		return
-	}
-	//w_b就是當前頁面的對象，通過該對象可以操作當前頁面了
-	//........
-	time.Sleep(5 * time.Minute)
-	return
+	fmt.Println(name)
 }
