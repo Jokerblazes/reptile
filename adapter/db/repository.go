86580a174867aee33870206aecc356354f064324
joker <cdzheng@thoughtworks.com>
2020-04-23 17:55:16 +0800

    return errors

--- adapter/db/repository.go
+++ adapter/db/repository.go
@@ -10,7 +10,7 @@ func (repository Repository) Pokemons() []model.Pokemon {
 	return nil
 }
 
-func (repository Repository) Save(pokemons []model.Pokemon) int {
+func (repository Repository) Save(pokemons []model.Pokemon) (int, []error) {
 	db := Db()
 	defer db.Close()
 	stmtIns, err := db.Prepare("INSERT INTO `pokemon` (`id`, `name`, `hp`, `attack`, `defense`, `speed`, `sp_atk`, `sp_def`, `height`, `weight`) " +
@@ -20,12 +20,13 @@ func (repository Repository) Save(pokemons []model.Pokemon) int {
 	}
 	defer stmtIns.Close()
 	failedNum := 0
+	var errs []error
 	for _, pokemon := range pokemons {
 		_, err := stmtIns.Exec(pokemon.Id, pokemon.Name, pokemon.Hp, pokemon.Attack, pokemon.Defense, pokemon.Speed, pokemon.SpAtk, pokemon.SpDef, pokemon.Height, pokemon.Weight)
 		if err != nil {
+			errs = append(errs, err)
 			failedNum++
-			panic(err.Error()) // proper error handling instead of panic in your app
 		}
 	}
-	return len(pokemons) - failedNum
+	return len(pokemons) - failedNum, errs
 }
--- adapter/modifier.go
+++ adapter/modifier.go
@@ -3,5 +3,5 @@ package adapter
 import "reptile/domain/model"
 
 type PokemonModifier interface {
-	savePokemons([]model.Pokemon) int
+	savePokemons([]model.Pokemon) (int, []error)
 }
